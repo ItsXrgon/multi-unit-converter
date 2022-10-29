789109f2c3dec8a5b968a1adba3b97fe760809f9
ItsXrgon <alikoheil2004@gmail.com>
2022-10-29 02:46:57 +0200

    created repository

---
+++ src/convertText.js
@@ -0,0 +1,15 @@
+class convertText {
+    time = "s" // Seconds
+    length = "m" // Meter
+    mass = "kg" // Kilogram
+    area = "m2" // Meter Squared
+    volume = "m3" // Meter Cubed (same for liquid volume)
+    speed = "m/s" // Meter Per Second
+    acceleration = "m/s2" // Meter Per Second Squared
+    temperature = "K" // Kelvin
+    electricCurrent = "A" // Ampere
+    thermodynamicTemp = "K" // Kelvin
+    amountOfSubstance = "mol" // Mole
+    luminousIntensity = "cd" // Candela
+    currency = "USD" // Dollars
+}
