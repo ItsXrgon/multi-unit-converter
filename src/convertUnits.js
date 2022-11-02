7df661186e91013ddd95004d5c77723cb4e76390
ItsXrgon <alikoheil2004@gmail.com>
2022-11-02 11:10:04 +0200

    Added Length and Weight conversions

--- src/convertUnits.js
+++ src/convertUnits.js
@@ -93,7 +93,7 @@ class convertUnits {
                     case ("cm"):
                         return value/10 + "cm";
                     case("in"):
-                        return value/3600 + "in";
+                        return value/25.4 + "in";
                     case("m"):
                         return value/1000 + "m";
                     case("km"):
@@ -101,242 +101,229 @@ class convertUnits {
                     case ("yd"):
                         return value/914.4 + "yd";
                     case ("ft"):
-                        return value/304.8 + "ft";
+                        feet = Math.floor(value/304.8) != 0 ? Math.floor(value/304.8) +"ft " : ""
+                        inch = (value/304.8)%12 != 0 ? (value/304.8)%12 + "in": ""
+                        return feet + inch;
                     case ("mi"):
                         return value/1609000 + "mi";
                 }
             case("m"): 
                 switch(unitOutput) {
                     case ("km"):
-                        return value/60 + "min";
+                        return value/1000 + "km";
                     case("ft"):
                         feet = Math.floor(value*3.28084) != 0 ? Math.floor(value*3.28084) +"ft " : ""
                         inch = (value*3.28084)%12 != 0 ? (value*3.28084)%12 + "in": ""
                         return feet + inch;
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("yd"):
+                        return value*1.094 + "yd";
+                    case("cm"):
+                        return value*100 + "cm";
+                    case ("mi"):
+                        return value*1609 + "mi";
+                    case ("in"):
+                        return value*39.37 + "in";
+                    case ("mm"):
+                        return value*1000 + "mm";
                 }
             case("km"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case ("mi"):
+                        return value/1.609 + "mi";
+                    case("m"):
+                        return value*1000 + "m";
+                    case("yd"):
+                        return value*1094 + "yd";
+                    case("ft"):
+                        return value*3281 + "ft";
+                    case ("in"):
+                        return value*39370 + "in";
+                    case ("cm"):
+                        return value/100000 + "cm";
+                    case ("mm"):
+                        return value*1000000 + "mm";
                 }
             case("ft"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case ("m"):
+                        return value/3.281+ "m";
+                    case("km"):
+                        return value/3281 + "km";
+                    case("in"):
+                        return value*12 + "in";
+                    case("mm"):
+                        return value*304.8 + "mm";
+                    case ("mi"):
+                        return value/5280 + "mi";
+                    case ("yd"):
+                        return value/3 + "yd";
+                    case ("cm"):
+                        return value*30.48 + "cm";
                 }
             case("in"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case ("ft"):
+                        return value/12 + "ft";
+                    case("m"):
+                        return value/39.37 + "in";
+                    case("cm"):
+                        return value*2.54 + "ms";
+                    case ("mm"):
+                        return value*25.4 + "mm";
+                    case("yd"):
+                        return value/36 + "yd";
+                    case ("km"):
+                        return value/39370 + "km";
+                    case ("mi"):
+                        return value/63360 + "mi";
                 }
             case("yd"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case ("ft"):
+                        feet = Math.floor(value*3) != 0 ? Math.floor(value*3) +"ft " : ""
+                        inch = (value*3)%12 != 0 ? (value*3)%12 + "in": ""
+                        return feet + inch;
+                    case("km"):
+                        return value/1094 + "km";
+                    case("mi"):
+                        return value/1760 + "mi";
+                    case("m"):
+                        return value/1.094 + "m";
+                    case ("in"):
+                        return value*36 + "in";
+                    case ("cm"):
+                        return value*91.44 + "cm";
+                    case ("mm"):
+                        return value*914.4 + "mm";
                 }
             case("mi"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case ("km"):
+                        return value*1.609 + "km";
+                    case("ft"):
+                        feet = Math.floor(value*63360) != 0 ? Math.floor(value*63360) +"ft " : ""
+                        inch = (value*63360)%12 != 0 ? (value*63360)%12 + "in": ""
+                        return feet + inch;
+                    case("yd"):
+                        return value*1760 + "yd";
+                    case("m"):
+                        return value*1609 + "m";
+                    case ("in"):
+                        return value*63360 + "in";
+                    case ("cm"):
+                        return value*160900 + "cm";
+                    case ("mm"):
+                        return value*1609000 + "mm";
                 }
         }        
     }
 
-    convertMass(value, unitInput, unitOutput) {
+    convertWeight(value, unitInput, unitOutput) {
         switch(unitInput) {
             case("g"): 
                 switch(unitOutput) {
                     case("kg"):
                         return value/1000 + "kg";
                     case ("mg"):
-                        return value/100 + "m";
-                    case("km"):
-                        return value/100000 + "km";
+                        return value*1000 + "mg";
                     case("oz"):
-                        return value/91.44 + "yd";
+                        return value/28.35 + "oz";
                     case ("lb"):
-                        return value/160900 + "mi";
+                        return value/453.6 + "lb";
                     case ("t"):
-                        return value/30.48 + "ft";
+                        return value/1000000 + "t";
                     case ("tn"):
-                        return value*10 + "mm";
+                        return value/1016046 + "tn";
                 }
             case("kg"): 
                 switch(unitOutput) {
-                    case ("cm"):
-                        return value/10 + "cm";
-                    case("in"):
-                        return value/3600 + "h";
-                    case("m"):
-                        return value/1000 + "m";
-                    case("km"):
-                        return value/1000000 + "km";
-                    case ("yd"):
-                        return value/914.4 + "yd";
-                    case ("ft"):
-                        return value/304.8 + "ft";
-                    case ("mi"):
-                        return value/1609000 + "mi";
+                    case("g"):
+                        return value*1000 + "g";
+                    case ("mg"):
+                        return value*1000000 + "mg";
+                    case("oz"):
+                        return value*35.274 + "oz";
+                    case ("lb"):
+                        return value*2.205 + "lb";
+                    case ("t"):
+                        return value/1000 + "t";
+                    case ("tn"):
+                        return value/1016 + "tn";
                 }
             case("lb"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("kg"):
+                        return value/2.205 + "kg";
+                    case ("mg"):
+                        return value*453600 + "mg";
+                    case("oz"):
+                        return value*16 + "oz";
+                    case ("g"):
+                        return value*453.6 + "g";
+                    case ("t"):
+                        return value/2205 + "t";
+                    case ("tn"):
+                        return value/2000 + "tn";
                 }
             case("oz"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("kg"):
+                        return value/35.274 + "kg";
+                    case ("mg"):
+                        return value*28350 + "mg";
+                    case("g"):
+                        return value*28.35 + "g";
+                    case ("lb"):
+                        return value/16 + "lb";
+                    case ("t"):
+                        return value/35270 + "t";
+                    case ("tn"):
+                        return value/32000 + "tn";
                 }
-            case("kg"): 
+            case("mg"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("g"):
+                        return value/1000 + "kg";
+                    case ("kg"):
+                        return value/1000000 + "kg";
+                    case("oz"):
+                        return value/28350 + "oz";
+                    case ("lb"):
+                        return value/453600 + "lb";
+                    case ("t"):
+                        return value/1000000000 + "t";
+                    case ("tn"):
+                        return value/907200000 + "tn";
                 }
             case("t"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("kg"):
+                        return value*1000 + "kg";
+                    case ("mg"):
+                        return value*1000000000 + "mg";
+                    case("oz"):
+                        return value*35270 + "oz";
+                    case ("lb"):
+                        return value*2205 + "lb";
+                    case ("g"):
+                        return value*1000000 + "g";
+                    case ("tn"):
+                        return value*1.102 + "tn";
                 }
             case("tn"): 
                 switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
-                }
-            case("oz"): 
-                switch(unitOutput) {
-                    case ("min"):
-                        return value/60 + "min";
-                    case("h"):
-                        return value/3600 + "h";
-                    case("ms"):
-                        return value*1000 + "ms";
-                    case("day"):
-                        return value/86400 + "days";
-                    case ("min"):
-                        return value/60 + "min";
-                    case ("min"):
-                        return value/60 + "min";
+                    case("kg"):
+                        return value*907.2 + "kg";
+                    case ("mg"):
+                        return value*907200000 + "mg";
+                    case("oz"):
+                        return value*32000 + "oz";
+                    case ("lb"):
+                        return value*2000 + "lb";
+                    case ("t"):
+                        return value/1.102 + "t";
+                    case ("g"):
+                        return value*907200 + "g";
                 }
         }        
     }
@@ -402,3 +389,5 @@ class convertUnits {
         }        
     }
 }
+
+export default convertUnits
