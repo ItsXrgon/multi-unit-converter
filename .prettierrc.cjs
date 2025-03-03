876bdb835601cd6c3decdbe4063622a7fb601dcd
itsXrgon <ali.koheil@gmail.com>
2025-03-03 04:11:40 +0200

    chore: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	semi: true,
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 120,
+	bracketSpacing: true,
+	endOfLine: 'lf',
+};
